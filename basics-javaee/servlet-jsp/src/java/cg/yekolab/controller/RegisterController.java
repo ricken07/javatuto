d5fe786f236d11327beae54121cb809ca5f0a6de
ricken07 <ricken.bazolo@gmail.com>
2017-10-27 23:30:36 +0100

    register control

--- basics-javaee/servlet-jsp/src/java/cg/yekolab/controller/RegisterController.java
+++ basics-javaee/servlet-jsp/src/java/cg/yekolab/controller/RegisterController.java
@@ -1,6 +1,6 @@
 package cg.yekolab.controller;
 
-
+import cg.yekolab.model.User;
 import interceptor.UserBean;
 import java.io.IOException;
 import javax.inject.Inject;
@@ -13,30 +13,47 @@ import javax.servlet.http.HttpServletResponse;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the edConnexionitor.
  */
-
 /**
  *
  * @author HP Notebook
  */
 @WebServlet(value = "/register")
-public class RegisterController extends HttpServlet{
+public class RegisterController extends HttpServlet {
 
     @Inject
     private UserBean userBean;
-    
+
     public RegisterController() {
     }
-    
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+
+        if (req.getParameter("btn1").equalsIgnoreCase("1")) {
+            //click sur le bouton Valider
+            User user = new User(userBean.allUsers().size() + 1,
+                    req.getParameter("nom"),
+                    req.getParameter("prenom"),
+                    req.getParameter("genre"),
+                    req.getParameter("login"),
+                    req.getParameter("pass"));
+            userBean.addUser(user);
+            req.setAttribute("message", "Enregistrement OK");
+            this.getServletContext().getRequestDispatcher("/register.jsp")
+                    .forward(req, resp);
+        } else {
+            //click sur le bouton Connexion (retour a la page de login)
+            //resp.sendRedirect(req.getContextPath()+"/login.jsp");
+            this.getServletContext().getRequestDispatcher("/login.jsp")
+                    .forward(req, resp);
+        }
     }
-    
+
 }
