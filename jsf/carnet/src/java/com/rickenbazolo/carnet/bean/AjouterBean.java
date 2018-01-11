2f0c7a23b0de38afccc64c9985cbd75c3f1bddb6
ricken07 <ricken.bazolo@gmail.com>
2018-01-11 16:15:36 +0100

    clean file

--- jsf/carnet/src/java/com/rickenbazolo/carnet/bean/AjouterBean.java
+++ jsf/carnet/src/java/com/rickenbazolo/carnet/bean/AjouterBean.java
@@ -18,14 +18,6 @@ import javax.inject.Named;
 @RequestScoped
 public class AjouterBean extends Contact {
 
-    /*private String nom;
-    private String prenom;
-    private String datenaiss;
-    private String genre;
-    private String telephone;
-    private String skype;
-    private String email;
-    private String adresse;*/
     @Inject
     private ContactBean bean;
 
@@ -34,14 +26,6 @@ public class AjouterBean extends Contact {
     }
 
     public String ajouter() {
-        /*System.out.println("Nom: " + this.nom);
-        System.out.println("Prenom: " + this.prenom);
-        System.out.println("Date de Naissance: " + this.datenaiss);
-        System.out.println("Genre: " + this.genre);
-        System.out.println("Téléphone: " + this.telephone);
-        System.out.println("Skype: " + this.skype);
-        System.out.println("Email: " + this.email);
-        System.out.println("Adresse: " + this.adresse);*/
         Contact contact = new Contact();
         contact.setAdresse(adresse);
         contact.setDatenaiss(datenaiss);
@@ -66,73 +50,7 @@ public class AjouterBean extends Contact {
         this.genre = "Masculin";
         this.telephone = "";
         this.skype = "";
-        this.email = "ricken.bazolo@gmail.com";
+        this.email = "";
         this.adresse = "";
     }
-
-    /*public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getDatenaiss() {
-        return datenaiss;
-    }
-
-    public void setDatenaiss(String datenaiss) {
-        this.datenaiss = datenaiss;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getSkype() {
-        return skype;
-    }
-
-    public void setSkype(String skype) {
-        this.skype = skype;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAdresse() {
-        return adresse;
-    }
-
-    public void setAdresse(String adresse) {
-        this.adresse = adresse;
-    }*/
-
-
 }
